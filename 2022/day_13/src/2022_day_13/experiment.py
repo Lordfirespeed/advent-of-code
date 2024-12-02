89091f160766e711a3202a7fc23da9fad2f0a2f7
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-12-02 22:28:53 +0000

    move 2022 test instance files to `test_instances` subdirs

--- 2022/day_13/src/2022_day_13/experiment.py
+++ 2022/day_13/src/2022_day_13/experiment.py
@@ -1,6 +1,6 @@
 from part_one import PacketParser, Solution
 
-with open("../../expected_test_output.txt") as expected_output_file:
+with open("../../test_instances/expected_test_output.txt") as expected_output_file:
     packets = [PacketParser.parse_string(line) for line in expected_output_file]
 
 solver = Solution([])
