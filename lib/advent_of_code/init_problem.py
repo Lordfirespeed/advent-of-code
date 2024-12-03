680078922b4df09cd98b619c86c167fa1ade8f9e
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-12-03 14:13:32 +0000

    add some QoL features to `advent_of_code init`

--- lib/advent_of_code/init_problem.py
+++ lib/advent_of_code/init_problem.py
@@ -1,11 +1,13 @@
 from pathlib import Path
 from typing import TypedDict
 
+from config import config
 from definitions import project_root_dir
 from skeleton_builder.generate import use_template
 
+from .api import advent_of_code_session
 from .args import AdventOfCodeArgNamespace
-
+from .scrape import get_problem_title
 
 problem_template_dir = Path(project_root_dir, "template")
 
@@ -20,4 +22,14 @@ async def init_problem(args: AdventOfCodeArgNamespace) -> None:
         "PROBLEM_YEAR": f"{args.problem_year:04}",
         "PROBLEM_DAY": f"{args.problem_day:02}",
     }
-    use_template(args.problem_dir, problem_template_dir, context)
+
+    if not args.problem_dir.exists():
+        use_template(args.problem_dir, problem_template_dir, context)
+
+    advent_of_code_session_token = config.get("ADVENT_OF_CODE_SESSION", None)
+    assert advent_of_code_session_token is not None
+    async with advent_of_code_session(advent_of_code_session_token) as session:
+        title = await get_problem_title(args, session)
+        print(f"--- {args.problem_year} Day {args.problem_day}: {title} ---")
+    
+    print(f"Briefing: https://adventofcode.com/{args.problem_year}/day/{args.problem_day}")
