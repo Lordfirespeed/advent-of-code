5f87c328d81325930aa44149151a72d9acf1eb3a
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-12-03 14:08:39 +0000

    move scraping code to `scrape_problem_title`

--- lib/advent_of_code/run.py
+++ lib/advent_of_code/run.py
@@ -2,14 +2,13 @@ from pathlib import Path
 import sys
 
 import aiohttp
-from bs4 import BeautifulSoup
 
 from config import config
 from problem_instance_abc import ProblemInstanceABC
 
-from .api import advent_of_code_session, fetch_problem_input, fetch_problem_html
+from .api import advent_of_code_session, fetch_problem_input
 from .args import AdventOfCodeArgNamespace
-from .scrape_problem_title import scrape_problem_title
+from .scrape_problem_title import get_problem_title
 
 async def load_problem_instance(args: AdventOfCodeArgNamespace, session: aiohttp.ClientSession) -> ProblemInstanceABC:
     instance_cache_path = Path(args.problem_dir, "cache", "input.txt")
@@ -24,35 +23,6 @@ async def load_problem_instance(args: AdventOfCodeArgNamespace, session: aiohttp
         instance_cache_handle.write(instance_plaintext)
     print("Fetched & cached problem input")
     return args.instance_type(instance_plaintext)
-
-
-async def get_problem_html_soup(args: AdventOfCodeArgNamespace, session: aiohttp.ClientSession) -> BeautifulSoup:
-    html_cache_path = Path(args.problem_dir, "cache", "problem.html")
-    if html_cache_path.exists():
-        with open(html_cache_path) as html_cache_handle:
-            return BeautifulSoup(html_cache_handle, features="html.parser")
-
-    problem_html = await fetch_problem_html(session, args.problem_year, args.problem_day)
-    html_cache_path.parent.mkdir(parents=True, exist_ok=True)
-    with open(html_cache_path, "w") as html_cache_handle:
-        html_cache_handle.write(problem_html)
-        html_cache_handle.write("\n")
-    return BeautifulSoup(problem_html, features="html.parser")
-
-
-async def get_problem_title(args: AdventOfCodeArgNamespace, session: aiohttp.ClientSession) -> str:
-    title_cache_path = Path(args.problem_dir, "cache", "title.txt")
-    if title_cache_path.exists():
-        with open(title_cache_path) as title_cache_handle:
-            return title_cache_handle.readline().strip()
-
-    soup = await get_problem_html_soup(args, session)
-    title = scrape_problem_title(soup)
-    title_cache_path.parent.mkdir(parents=True, exist_ok=True)
-    with open(title_cache_path, "w") as title_cache_handle:
-        title_cache_handle.write(title)
-        title_cache_handle.write("\n")
-    return title
 
 
 async def run(args: AdventOfCodeArgNamespace) -> None:
--- lib/advent_of_code/scrape_problem_title.py
+++ lib/advent_of_code/scrape_problem_title.py
@@ -1,4 +1,39 @@
+from pathlib import Path
+
+import aiohttp
 from bs4 import BeautifulSoup
+
+from advent_of_code.api import fetch_problem_html
+from advent_of_code.args import AdventOfCodeArgNamespace
+
+
+async def get_problem_html_soup(args: AdventOfCodeArgNamespace, session: aiohttp.ClientSession) -> BeautifulSoup:
+    html_cache_path = Path(args.problem_dir, "cache", "problem.html")
+    if html_cache_path.exists():
+        with open(html_cache_path) as html_cache_handle:
+            return BeautifulSoup(html_cache_handle, features="html.parser")
+
+    problem_html = await fetch_problem_html(session, args.problem_year, args.problem_day)
+    html_cache_path.parent.mkdir(parents=True, exist_ok=True)
+    with open(html_cache_path, "w") as html_cache_handle:
+        html_cache_handle.write(problem_html)
+        html_cache_handle.write("\n")
+    return BeautifulSoup(problem_html, features="html.parser")
+
+
+async def get_problem_title(args: AdventOfCodeArgNamespace, session: aiohttp.ClientSession) -> str:
+    title_cache_path = Path(args.problem_dir, "cache", "title.txt")
+    if title_cache_path.exists():
+        with open(title_cache_path) as title_cache_handle:
+            return title_cache_handle.readline().strip()
+
+    soup = await get_problem_html_soup(args, session)
+    title = scrape_problem_title(soup)
+    title_cache_path.parent.mkdir(parents=True, exist_ok=True)
+    with open(title_cache_path, "w") as title_cache_handle:
+        title_cache_handle.write(title)
+        title_cache_handle.write("\n")
+    return title
 
 
 def scrape_problem_title(problem_html_soup: BeautifulSoup) -> str:
