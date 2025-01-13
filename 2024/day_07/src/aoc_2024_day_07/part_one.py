d0a5706412a2cb7f8be7130b4efa550acad8f066
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-01-13 13:10:26 +0000

    complete 2024 day 7

---
+++ 2024/day_07/src/aoc_2024_day_07/part_one.py
@@ -0,0 +1,68 @@
+from itertools import product
+from typing import Protocol, Sequence
+
+from util.exceptions.no_solution_error import NoSolutionError
+
+from .problem import ProblemInstance, CalibrationEquation
+
+
+class Operator(Protocol):
+    def apply(self, a: int, b: int) -> int: ...
+
+
+class Add(Operator):
+    def apply(self, a: int, b: int) -> int:
+        return a + b
+    
+    def __repr__(self):
+        return "+"
+
+
+class Multiply(Operator):
+    def apply(self, a: int, b: int) -> int:
+        return a * b
+    
+    def __repr__(self):
+        return "*"
+
+
+operators = [
+    Add(), Multiply(),
+]
+
+type OperatorAssignment = Sequence[Operator]
+
+
+def try_operator_assignment(equation: CalibrationEquation, assignment: OperatorAssignment) -> bool:
+    assert len(equation.operands) == len(assignment) + 1
+    accumulator = equation.operands[0]
+    for operator, operand in zip(assignment, equation.operands[1:]):
+        accumulator = operator.apply(accumulator, operand)
+    return accumulator == equation.result
+
+
+def brute_force_operator_assignment(equation: CalibrationEquation) -> OperatorAssignment:
+    operator_count = len(equation.operands) - 1
+    for assignment in product(operators, repeat=operator_count):
+        assignment_works = try_operator_assignment(equation, assignment)
+        if assignment_works:
+            return assignment
+
+    raise NoSolutionError
+
+
+def is_possibly_true(equation: CalibrationEquation) -> bool:
+    try: 
+        brute_force_operator_assignment(equation)
+        return True
+    except NoSolutionError:
+        return False
+
+
+class PartOneSolver:
+    def __init__(self, instance: ProblemInstance) -> None:
+        self.instance = instance
+
+    def solve(self) -> int:
+        possibly_true_equations = filter(is_possibly_true, self.instance.equations)
+        return sum(equation.result for equation in possibly_true_equations)
---
+++ 2024/day_07/src/aoc_2024_day_07/problem.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+from typing import Self
+
+from problem_instance_abc import ProblemInstanceABC
+
+
+@dataclass(frozen=True)
+class CalibrationEquation:
+    result: int
+    operands: tuple[int, ...]
+    
+    @classmethod
+    def from_plaintext(cls, plaintext: str) -> Self:
+        parts = plaintext.split(": ")
+        assert len(parts) == 2
+        result_str, operands_str = parts
+        result = int(result_str)
+        operands = tuple(int(operand) for operand in operands_str.split())
+        return cls(result, operands)
+
+
+class ProblemInstance(ProblemInstanceABC):
+    equations: tuple[CalibrationEquation, ...]
+    
+    def parse_plaintext(self) -> None:
+        self.equations = tuple(CalibrationEquation.from_plaintext(line) for line in self.input_plaintext.splitlines())
