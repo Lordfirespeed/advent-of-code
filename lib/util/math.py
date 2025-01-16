4d665883a5910e64ef44622618e36f536ced98fe
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-01-16 15:10:28 +0000

    add isqrt_ceil

--- lib/util/math.py
+++ lib/util/math.py
@@ -1,4 +1,25 @@
-from math import prod, sumprod
+from math import (
+    isqrt as isqrt_floor,
+    prod,
+    sumprod,
+)
+
+
+__all__ = (
+    "isqrt_floor",
+    "isqrt_ceil",
+    "floor_div",
+    "ceil_div",
+    "prod",
+    "sumprod",
+)
+
+
+def isqrt_ceil(n: int) -> int:
+    """
+    https://docs.python.org/3/library/math.html#math.isqrt
+    """
+    return 1 + isqrt_floor(n - 1)
 
 
 def floor_div(a,b):
