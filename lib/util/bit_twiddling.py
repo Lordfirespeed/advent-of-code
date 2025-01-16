efbff37826b315e9acf0daa27deedb013d3b90db
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-01-16 22:41:09 +0000

    feat: support any type implementing `__int__`

--- lib/util/bit_twiddling.py
+++ lib/util/bit_twiddling.py
@@ -3,24 +3,28 @@ from typing import (
     Literal,
     SupportsBytes,
     SupportsIndex, 
+    SupportsInt,
 )
 
 
 type BytesLike = Iterable[SupportsIndex] | SupportsBytes
 
 
-def byte_length(value: int) -> int:
+def byte_length(value: SupportsInt) -> int:
+    value = int(value)
     assert value > 0
     # 2^3 = 8, so right-shifting 3 times is equivalent to dividing by 8
     return ((value.bit_length() - 1) >> 3) + 1
 
 
-def signed_byte_length(value: int) -> int:
+def signed_byte_length(value: SupportsInt) -> int:
+    value = int(value)
     # 2^3 = 8, so right-shifting 3 times is equivalent to dividing by 8
     return (value.bit_length() >> 3) + 1
 
 
-def signed_bin(value: int) -> str:
+def signed_bin(value: SupportsInt) -> str:
+    value = int(value)
     binary_byte_length = signed_byte_length(value)
     binary_bit_length = binary_byte_length * 8
     signed_value_bytes = value.to_bytes(length=binary_byte_length, signed=True)
@@ -28,12 +32,14 @@ def signed_bin(value: int) -> str:
     return f"{unsigned_value_integer:0{binary_bit_length}b}"
 
 
-def last_set_bit_index(binary: int | BytesLike) -> int | Literal[-1]:
+def last_set_bit_index(binary: SupportsInt | BytesLike) -> int | Literal[-1]:
     """
     Compute the index (from the right) of the right-most '1' bit in 'binary'.
     For integer powers of 2, this computes log_2(binary).
     If there exists no index (because the input contains no set bits), returns -1.
     """
+    if isinstance(binary, SupportsInt):
+        binary = int(binary)
     if type(binary) is not int:
         binary = int.from_bytes(binary, byteorder="big")
 
@@ -50,6 +56,8 @@ def first_set_bit_index(binary: int | BytesLike) -> int | Literal[-1]:
     Compute the index (from the right) of the left-most '1' bit in 'binary'.
     If there exists no index (because the input contains no set bits), returns -1.
     """
+    if isinstance(binary, SupportsInt):
+        binary = int(binary)
     if type(binary) is not int:
         binary = int.from_bytes(binary, byteorder="big")
     
