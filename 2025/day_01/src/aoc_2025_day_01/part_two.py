b80a937b6b40510b3a155dcd56e8e224b7e8716f
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-12-04 00:50:58 +0000

    complete day 1 part two

--- 2025/day_01/src/aoc_2025_day_01/part_two.py
+++ 2025/day_01/src/aoc_2025_day_01/part_two.py
@@ -1,9 +1,26 @@
-from .problem import ProblemInstance
+from .problem import ProblemInstance, Turn, TurnDirection
 
 
 class PartTwoSolver:
     def __init__(self, instance: ProblemInstance) -> None:
         self.instance = instance
+        self.dial_position = 50
+        self.zero_count = 0
+
+    def do_turn(self, turn: Turn) -> None:
+        sign = +1 if turn.direction is TurnDirection.Right else -1
+        old_position = self.dial_position
+        self.dial_position += sign * turn.distance
+        overflows, new_position = divmod(self.dial_position, 100)
+        if overflows < 0 and old_position == 0:
+            overflows += 1
+        self.zero_count += abs(overflows)
+        self.dial_position = new_position
+        if overflows <= 0 and new_position == 0:
+            self.zero_count += 1
 
     def solve(self) -> int:
-        return len(self.instance.input_plaintext)
+        for turn in self.instance.turns:
+            self.do_turn(turn)
+
+        return self.zero_count
