0cf56f2a22e99c5d53f2e5c23b168b85402424ea
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-12-04 02:50:01 +0000

    solve 2025.03.02

--- 2025/day_03/src/aoc_2025_day_03/part_two.py
+++ 2025/day_03/src/aoc_2025_day_03/part_two.py
@@ -1,9 +1,28 @@
-from .problem import ProblemInstance
+from .problem import ProblemInstance, BatteryBank, BatteryJoltage
 
 
 class PartTwoSolver:
     def __init__(self, instance: ProblemInstance) -> None:
         self.instance = instance
 
+    def max_joltage(self, bank: BatteryBank) -> BatteryJoltage:
+        selection = bank[-12:]
+
+        for index in reversed(range(len(bank) - 12)):
+            digit = bank[index]
+            for compare_index in range(len(selection)):
+                compare_digit = selection[compare_index]
+                if compare_digit > digit: break
+                selection[compare_index] = digit
+                digit = compare_digit
+
+        selection_joltage = int("".join((str(joltage) for joltage in selection)))
+
+        return BatteryJoltage(selection_joltage)
+
     def solve(self) -> int:
-        return len(self.instance.input_plaintext)
+        total_output_joltage = 0
+        for bank in self.instance.banks:
+            bank_output = self.max_joltage(bank)
+            total_output_joltage += bank_output
+        return total_output_joltage
